0ea6bf5e64a2786562d5a3141132bab9b54bb8fa
Titus Wormer <tituswormer@gmail.com>
2023-05-10 17:10:19 +0200

    Refactor types

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 /**
- * @type {import('micromark-util-types').Extension}
+ * @typedef {import('micromark-util-types').Extension} Extension
+ */
+
+// To do: next major: expose functions.
+
+/**
+ * @type {Extension}
  */
 export const mdxMd = {
   disable: {null: ['autolink', 'codeIndented', 'htmlFlow', 'htmlText']}

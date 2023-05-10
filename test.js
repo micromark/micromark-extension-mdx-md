5d68919c5654ed882036d9ac2be188f031a86266
Titus Wormer <tituswormer@gmail.com>
2023-05-10 17:13:49 +0200

    Use Node test runner

--- test.js
+++ test.js
@@ -1,61 +1,60 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {micromark} from 'micromark'
-import {mdxMd as syntax} from './index.js'
+import {mdxMd} from './index.js'
 
-test('micromark-extension-mdx-md', (t) => {
-  t.equal(
-    micromark('<a>', {extensions: [syntax], allowDangerousHtml: true}),
+test('micromark-extension-mdx-md', function () {
+  assert.equal(
+    micromark('<a>', {extensions: [mdxMd], allowDangerousHtml: true}),
     '<p>&lt;a&gt;</p>',
     'should turn off html (flow)'
   )
 
-  t.equal(
-    micromark('a<b>', {extensions: [syntax], allowDangerousHtml: true}),
+  assert.equal(
+    micromark('a<b>', {extensions: [mdxMd], allowDangerousHtml: true}),
     '<p>a&lt;b&gt;</p>',
     'should turn off html (text)'
   )
 
-  t.equal(
-    micromark('a <https://example.com>', {extensions: [syntax]}),
+  assert.equal(
+    micromark('a <https://example.com>', {extensions: [mdxMd]}),
     '<p>a &lt;https://example.com&gt;</p>',
     'should turn off autolinks'
   )
 
-  t.equal(
-    micromark('    a', {extensions: [syntax]}),
+  assert.equal(
+    micromark('    a', {extensions: [mdxMd]}),
     '<p>a</p>',
     'should turn off indented code (1, basic)'
   )
 
-  t.equal(
-    micromark('    - a\n    - b', {extensions: [syntax]}),
+  assert.equal(
+    micromark('    - a\n    - b', {extensions: [mdxMd]}),
     '<ul>\n<li>a</li>\n<li>b</li>\n</ul>',
     'should turn off indented code (2, containers)'
   )
 
-  t.equal(
-    micromark('    - a\n\n    - b', {extensions: [syntax]}),
+  assert.equal(
+    micromark('    - a\n\n    - b', {extensions: [mdxMd]}),
     '<ul>\n<li>\n<p>a</p>\n</li>\n<li>\n<p>b</p>\n</li>\n</ul>',
     'should turn off indented code (3, containers)'
   )
 
-  t.equal(
-    micromark('    > a\n    > b', {extensions: [syntax]}),
+  assert.equal(
+    micromark('    > a\n    > b', {extensions: [mdxMd]}),
     '<blockquote>\n<p>a\nb</p>\n</blockquote>',
     'should turn off indented code (4, containers)'
   )
 
-  t.equal(
-    micromark('a\n   ---', {extensions: [syntax]}),
+  assert.equal(
+    micromark('a\n   ---', {extensions: [mdxMd]}),
     '<h2>a</h2>',
     'should turn off indented code (5, interrupt)'
   )
 
-  t.equal(
-    micromark('a\n    ---', {extensions: [syntax]}),
+  assert.equal(
+    micromark('a\n    ---', {extensions: [mdxMd]}),
     '<h2>a</h2>',
     'should turn off indented code (6, interrupt)'
   )
-
-  t.end()
 })
